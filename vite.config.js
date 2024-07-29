74307003255aabc12feadc470969db0f2986991e
Maojin-juan <edwardelric199887@gmail.com>
2024-07-30 01:15:11 +0800

    tailwind-template

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
-import { ViteEjsPlugin } from "vite-plugin-ejs";
-import liveReload from "vite-plugin-live-reload";
-
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from "path";
+import { fileURLToPath } from "url";
 import { globSync } from "glob";
+import liveReload from "vite-plugin-live-reload";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 function moveOutputPlugin() {
   return {
@@ -23,27 +24,24 @@ function moveOutputPlugin() {
 }
 
 export default defineConfig(({ mode }) => {
-  //載入環境變數
-  //mode：指定當前的模式（例如 'development'、'production' 等）。
-  //process.cwd()：返回 Node.js 程序的當前工作目錄，通常是專案的根目錄。
   const env = loadEnv(mode, process.cwd());
 
-  //設定基礎路徑，開發模式下為 "/"，生產模式下為 env.viteBase 或 "/"
-  const base = mode === "development" ? "/" : env.viteBase || "/";
-
   return {
-    base,
+    base: mode === "development" ? "/" : env.viteBase || "/",
     build: {
       rollupOptions: {
-        input: Object.fromEntries(
-          globSync("src/pages/**/*.html").map((file) => [
-            path.relative(
-              "src/pages",
-              file.slice(0, -path.extname(file).length),
-            ),
-            fileURLToPath(new URL(file, import.meta.url)),
-          ]),
-        ),
+        input: {
+          main: path.resolve(__dirname, "src/pages/index.html"),
+          ...Object.fromEntries(
+            globSync("src/pages/**/*.html").map((file) => [
+              path.relative(
+                "src/pages",
+                file.slice(0, file.length - path.extname(file).length),
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ]),
+          ),
+        },
       },
       outDir: "dist",
     },
@@ -55,8 +53,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 5487,
       open: "/src/pages/index.html",
-
-      // fs.allow 是一個允許訪問的目錄列表。這些目錄是通過 path.resolve 方法解析出來的絕對路徑。
       fs: {
         allow: [
           path.resolve(__dirname, "src"),
@@ -71,9 +67,14 @@ export default defineConfig(({ mode }) => {
         "./src/pages/**/*.html",
       ]),
       ViteEjsPlugin({
-        base: mode === "development" ? "/src/pages/" : base,
+        base: mode === "development" ? "/src/pages/" : env.viteBase || "/",
       }),
       moveOutputPlugin(),
     ],
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer],
+      },
+    },
   };
 });
