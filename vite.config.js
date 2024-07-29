73bdfa81264eb7a03a0d8655b551b140048bd024
Maojin-juan <edwardelric199887@gmail.com>
2024-07-30 01:36:24 +0800

    tailwind-template

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
+import liveReload from "vite-plugin-live-reload";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 import { glob } from "glob";
-
-import liveReload from "vite-plugin-live-reload";
 
 function moveOutputPlugin() {
   return {
@@ -23,7 +22,7 @@ function moveOutputPlugin() {
 }
 
 export default defineConfig({
-  base: "/hexWeek5/",
+  base: "/tailwind-template/",
   plugins: [
     liveReload([
       "./src/layout/**/*.ejs",
@@ -33,8 +32,12 @@ export default defineConfig({
     ViteEjsPlugin(),
     moveOutputPlugin(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
-    port: 5487,
     // 啟動 server 時預設開啟的頁面
     open: "src/pages/index.html",
   },
